e05b29e970097c667c7a23f437f36e30426e0c08
Matsushiro <matsusiro12kousei@gmail.com>
2023-08-31 11:54:46 +0900

    home.html表示する過程2

--- src/main/java/jp/gihyo/projava/tasklist/HomeController.java
+++ src/main/java/jp/gihyo/projava/tasklist/HomeController.java
@@ -10,6 +10,7 @@ import org.springframework.web.bind.annotation.ResponseBody;
 import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.UUID;
 
 @Controller
 public class HomeController {
@@ -43,5 +44,13 @@ public class HomeController {
     String listItems(Model model) {
         model.addAttribute("taskList", taskItems);
         return "home";
+    }
+    @GetMapping("/add")
+    String addItem(@RequestParam("task")String task,
+                   @RequestParam("deadline")String deadline){
+        String id = UUID.randomUUID().toString().substring(0,8);
+        TaskItem item = new TaskItem(id,task,deadline,false);
+        taskItems.add(item);
+        return "redirect:/list";
     }
 }
