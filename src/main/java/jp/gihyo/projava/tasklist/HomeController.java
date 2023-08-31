e36de4e9415ca523436d67cd64686f6320f3a06a
m-nakan70 <mio.nakanishi70@gmail.com>
2023-08-31 14:30:53 +0900

    修正_14:30

--- src/main/java/jp/gihyo/projava/tasklist/HomeController.java
+++ src/main/java/jp/gihyo/projava/tasklist/HomeController.java
@@ -1,6 +1,5 @@
 package jp.gihyo.projava.tasklist;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.Banner;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
